f49ede52dd3a54637c3ebe374428887f2f298f68
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-09 02:26:27 +0530

    Basic query

--- bbq.lisp
+++ bbq.lisp
@@ -4,5 +4,10 @@
 
 ;;; "bbq" goes here. Hacks and glory await!
 
-(defun do-things ()
-  (print "Phrasing"))
+(defun basic (query)
+  "Basic query unifying the mpc interface"
+  (inferior-shell:run/ss '(mpc clear))
+  (inferior-shell:run/ss (list 'mpc 'searchadd 'artist query))
+  (inferior-shell:run/ss (list 'mpc 'searchadd 'album query))
+  (inferior-shell:run/ss (list 'mpc 'searchadd 'title query))
+  (inferior-shell:run/ss '(mpc play)))
