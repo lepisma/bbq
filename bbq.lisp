6f3f6ab9304d0736ef78b42e96f24a2172fbd43f
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-09 12:38:29 +0530

    Better basic search

--- bbq.lisp
+++ bbq.lisp
@@ -4,10 +4,29 @@
 
 ;;; "bbq" goes here. Hacks and glory await!
 
-(defun basic (query)
-  "Basic query unifying the mpc interface"
+(defun search-basic (query)
+  "Simple search across album, artist and title"
+  (let ((fields '(album artist title)))
+    (remove-if (lambda (item) (string-equal "" item))
+               (reduce
+                (lambda (a b)
+                  (union a b :test 'string-equal))
+                (mapcar (lambda (field)
+                          (cl-strings:split
+                           (inferior-shell:run/ss `(mpc search ,field ,query)) #\Linefeed)) fields)))))
+
+(defun mpc-add (items)
+  "Add items to the playlist"
+  (mapc (lambda (item) (inferior-shell:run/ss `(mpc add ,item))) items))
+
+(defun mpc-clear-play (items)
+  "Clear playlist. Add items and play."
   (inferior-shell:run/ss '(mpc clear))
-  (inferior-shell:run/ss (list 'mpc 'searchadd 'artist query))
-  (inferior-shell:run/ss (list 'mpc 'searchadd 'album query))
-  (inferior-shell:run/ss (list 'mpc 'searchadd 'title query))
+  (mpc-add items)
+  (inferior-shell:run/ss '(mpc play)))
+
+(defun mpc-clear-play-all ()
+  "Play all items"
+  (inferior-shell:run/ss '(mpc clear))
+  (inferior-shell:run/ss "mpc ls | mpc add")
   (inferior-shell:run/ss '(mpc play)))
