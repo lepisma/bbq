79401dc7eb5b5c9caf43cbdbc1140a800fd9a343
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-08-11 00:24:31 +0530

    Basic support for new

--- bbq.lisp
+++ bbq.lisp
@@ -2,9 +2,14 @@
 
 (in-package #:bbq)
 
-;;; "bbq" goes here. Hacks and glory await!
+(export '(mpc-clear-play
+          search-items
+          new-items))
 
-(defun search-basic (query)
+(defvar *music-dir* "/media/lepisma/Data/Music/")
+(defvar *beets-db* "/media/lepisma/Data/Music/beets.db")
+
+(defun search-items (query)
   "Simple search across album, artist and title"
   (let ((fields '(album artist title)))
     (remove-if (lambda (item) (string-equal "" item))
@@ -15,12 +20,19 @@
                           (cl-strings:split
                            (inferior-shell:run/ss `(mpc search ,field ,query)) #\Linefeed)) fields)))))
 
-(defun mpc-add (items)
+(defun new-items (n)
+  "Return n new items"
+  (let* ((stmt (format nil "SELECT substr(path, ~A) FROM items ORDER BY mtime DESC LIMIT ~A" (+ 1 (length *music-dir*)) n))
+         (results (inferior-shell:run/ss `(sqlite3 ,*beets-db* ,stmt))))
+    (cl-strings:split results #\Linefeed)))
+
+(defun mpc-add-items (items)
   "Add items to the playlist"
+  (mapcar #'print items)
   (inferior-shell:run/ss `(mpc add ,@items)))
 
 (defun mpc-clear-play (items)
   "Clear playlist. Add items and play."
   (inferior-shell:run/ss '(mpc clear))
-  (mpc-add items)
+  (mpc-add-items items)
   (inferior-shell:run/ss '(mpc play)))
