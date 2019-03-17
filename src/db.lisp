1c2519d9793eb78b78532fb3640c2ddec201babf
Abhinav Tushar <lepisma@fastmail.com>
2019-03-18 00:06:47 +0530

    song struct and generic for present?

--- src/db.lisp
+++ src/db.lisp
@@ -14,15 +14,29 @@
   `(with-open-database (*db* *db-path*)
      ,@body))
 
-(defstruct song
-  id artist title album url mtime)
-
 (defun add-alist (table alist)
   "Add alist in the table."
   (with-db
       (let ((keys (cl-strings:join (mapcar #'car alist) :separator ", "))
             (plcs (cl-strings:join (serapeum:repeat-sequence '("?") (length alist)) :separator ", ")))
         (apply #'execute-non-query *db* #?"INSERT INTO ${table} (${keys}) VALUES (${plcs})" (mapcar #'cdr alist)))))
+
+
+
+(defstruct song
+  id
+  artist
+  title
+  album
+  url
+  (mtime (serapeum:get-unix-time) :type integer))
+
+(defmethod present? ((s song))
+  "Tell if the given song is present in the db."
+  (if (and (song-artist s) (song-title s))
+      (with-db
+          (execute-to-list *db* "SELECT * FROM songs WHERE artist = ? AND title = ?" (song-artist s) (song-title s)))
+      (error "song underspecified")))
 
 (defun add-song (artist title url &key album)
   "Unconditionally add item in the database.
