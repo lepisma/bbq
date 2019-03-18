e799c7cf6b21aa2f07866928bfbf9cd209a0ee67
Abhinav Tushar <lepisma@fastmail.com>
2019-03-18 23:06:30 +0530

    clean up log parser

--- src/log.lisp
+++ src/log.lisp
@@ -2,58 +2,33 @@
 
 (in-package #:bbq-log)
 
-#|
-(defun attr-init-sym (attr-form)
-  (let ((name (if (listp attr-form) (car attr-form) attr-form)))
-    (intern (symbol-name name) "KEYWORD")))
+(defvar *player-log* (truename "~/.mpm.d/play-log")
+  "Log file of player.")
 
-(defmacro defclass+ (name &optional (attrs ()))
-  "No boilerplate!"
-  `(defclass ,name ()
-     ,(mapcar (lambda (attr)
-                (if (listp attr)
-                    (if (member :initarg attr)
-                        attr
-                        (append attr `(:initarg ,(attr-init-sym attr))))
-                    (list attr :initarg (attr-init-sym attr))))
-       attrs)))
-|#
+(defvar *last-fm-log* (truename "~/.mpm.d/last-fm.csv")
+  "Last fm dump saved using https://benjaminbenben.com/lastfm-to-csv/.
+The time format there is something like '16 Sep 2018 10:49'. Time zone looks
+like UTC.")
 
-(defstruct entry
-  id timestamp artist title album)
-
-(defun line-empty-p (line)
+(defun line-empty? (line)
   (string-equal "" (string-trim '(#\ ) line)))
 
 (defun read-lines (file-path)
   (with-open-file (fp file-path)
     (loop for line = (read-line fp nil)
-          while (and line (not (line-empty-p line)))
-          collect line)))
+          while line when (not (line-empty? line)) collect line)))
 
-(defun read-player-log (&optional (file-path (truename "~/.mpm.d/play-log")))
+(defun read-player-log (&optional (file-path *player-log*))
   "Read logs from the music player itself."
-  (let ((lines (read-lines file-path)))
-    (mapcar (lambda (line)
-              (ematch (cl-strings:split line #\,)
-                ((list timestamp id) (make-entry
-                                      :id (parse-integer id)
-                                      :timestamp (parse-integer timestamp)))))
-            lines)))
+  (mapcar (lambda (line) (apply #'cons (mapcar #'parse-integer (cl-strings:split line #\,))))
+          (read-lines file-path)))
 
-(defun parse-lastfm-timestring (timestring)
-  "Convert to timestamp. TODO:"
-  timestring)
-
-(defun read-lastfm-log (&optional (file-path (truename "~/.mpm.d/last-fm.csv")))
-  "Read lastfm dump saved using https://benjaminbenben.com/lastfm-to-csv/.
-The time format there is something like '16 Sep 2018 10:49'. Time zone looks like UTC."
-  (let ((lines (read-lines file-path)))
-    (mapcar (lambda (line)
-              (ematch (cl-strings:split line #\,)
-                ((list artist album title timestring)
-                 (make-entry :timestamp (parse-lastfm-timestring timestring)
-                             :artist artist
-                             :title title
-                             :album (unless (string-equal "" album) album)))))
-            lines)))
+(defun read-lastfm-log (&optional (file-path *last-fm-log*))
+  (mapcar (lambda (line)
+            (ematch (cl-strings:split line #\,)
+              ((list artist album title timestring)
+               `(:artist ,artist
+                 :album ,(unless (string-equal "" album) album)
+                 :title ,title
+                 :timestring ,timestring))))
+          (read-lines file-path)))
