5f89cf742dac50f26a0a85767f673c4c070d5bec
Abhinav Tushar <lepisma@fastmail.com>
2019-03-18 22:52:28 +0530

    remove unused cli vars

--- src/cli.lisp
+++ src/cli.lisp
@@ -1,10 +1,3 @@
 ;; Functions for cli usage
 
 (in-package #:bbq)
-
-(defparameter known-commands
-  '(":new" ":cap" ":next" ":prev" ":toggle" ":state" ":repeat")
-  "Commands known to bbq")
-
-(defparameter known-flags '("--list" "--sexp")
-  "Flags known to bbq")
