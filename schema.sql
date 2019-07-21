08d2e736fb0df13fd752adb6c7cb38b414ebc76d
Abhinav Tushar <lepisma@fastmail.com>
2019-07-21 22:22:42 +0530

    play log table

--- schema.sql
+++ schema.sql
@@ -8,4 +8,12 @@ CREATE TABLE songs (
 CREATE TABLE sources (
 	id INTEGER NOT NULL, resolver TEXT, name TEXT, url TEXT, inc BOOLEAN,
 	PRIMARY KEY (id)
-)
+);
+
+-- Log of songs played
+CREATE TABLE play_log (
+  id INTEGER PRIMARY KEY,
+  time INTEGER NOT NULL,
+  song_id INTEGER NOT NULL,
+  FOREIGN KEY(song_id) REFERENCES songs(id)
+);
